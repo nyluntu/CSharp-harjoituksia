4fe2793b96e97f3f34933988d7312c3dd3995d0c
Turo Nylund <turo.nylund@outlook.com>
2015-10-08 14:47:01 +0300

    lisatty publicit ja muutettu metodi sana funktioksi.

--- FunktiotHarjoituksia/FunktiotHarjoituksia/Program.cs
+++ FunktiotHarjoituksia/FunktiotHarjoituksia/Program.cs
@@ -8,49 +8,49 @@ namespace FunktiotHarjoituksia
     class Program
     {
 
-        // Main -metodi (funktio, aliohjelma), jota kutsutaan ohjelman
+        // Main -funktio (funktio, aliohjelma), jota kutsutaan ohjelman
         // käynnistyessä. Saa parametrina merkkijono-taulukon
-        // ja ei palauta mitään, koska on void -tyyppinen metodi.
+        // ja ei palauta mitään, koska on void -tyyppinen funktio.
         static void Main(string[] args)
         {
             string tyontekija = "Pekka Porkkana";
             int varattujaPoytia = 0;
             int vieraidenMaara = 0;
 
-            // Kutsutaan TulostaOhjelmanNimi-metodia. Katso metodin
-            // kuvauksesta mitä tekee. Esimerkki yksinkertaisesta metodista,
+            // Kutsutaan TulostaOhjelmanNimi-funktiota. Katso funktion
+            // kuvauksesta mitä tekee. Esimerkki yksinkertaisesta funktiosta,
             // joka ei palauta mitään tai ota parametreja.
             TulostaOhjelmanNimi();
             Console.WriteLine("------------------------------------");
 
-            // Kutsutaan HaePaikkojenKokonaismaara-metodia. Katso
-            // metodin kuvauksesta mitä tekee. Esimerkki arvon palauttavasta
-            // metodista mutta ei ota parametreja.
+            // Kutsutaan HaePaikkojenKokonaismaara-funktiota. Katso
+            // funktion kuvauksesta mitä tekee. Esimerkki arvon palauttavasta
+            // funktiosta mutta ei ota parametreja.
             int poytiaYhteensa = HaePoytienKokonaismaara();
             Console.WriteLine("Paikkoja yhteensa: " + poytiaYhteensa + " kpl");
 
             // Vaihtoehtoinen tapa edelliselle tulostukselle, jossa käytetään
-            // metodia, joka palauttaa eri tyyppisen paluuarvon. 
+            // funktiota, joka palauttaa eri tyyppisen paluuarvon. 
             string poytienMaara = PalautaPoytienMaara();
             Console.WriteLine(poytienMaara);
 
-            // Kutsutaan TulostaUudelleRiville-metodia. Katso metodin 
+            // Kutsutaan TulostaUudelleRiville-funktiota. Katso funktion 
             // kuvauksesta mitä tekee. Esimerkki parametria käyttävästä 
-            // metodista, jolle annetaan tietoa mutta ei palauta mitään.
+            // funktiosta, jolle annetaan tietoa mutta ei palauta mitään.
             //
-            // Parametrillinen metodi voi myös palauttaa arvon yllä
+            // Parametrillinen funktio voi myös palauttaa arvon yllä
             // esitettyjen esimerkkien mukaisesti.
             TulostaUudelleRiville("------------------------------------");
             TulostaUudelleRiville("Työntekija: " + tyontekija);
             TulostaUudelleRiville("Kello: " + DateTime.Now.ToShortTimeString());
             TulostaUudelleRiville("------------------------------------");
 
-            // Kutsutaan KasvataVarattujaPoytia-metodia. Katso metodin
-            // kuvauksesta mitä tekee. Esimerkki metodista, jolle annetaan
+            // Kutsutaan KasvataVarattujaPoytia-funktiota. Katso funktion
+            // kuvauksesta mitä tekee. Esimerkki funktiosta, jolle annetaan
             // parametri, suorittaa tehtävän ja palauttaa arvon.
             // 
-            // Metodin kuvauksessa on kuitenkin kerrottu huomioita, mitä 
-            // asioita tässä halutaan korostaan. Metodia kutsutaan mallin
+            // Munktion kuvauksessa on kuitenkin kerrottu huomioita, mitä 
+            // asioita tässä halutaan korostaan. funktiota kutsutaan mallin
             // vuoksi useampia kertoja.
             TulostaUudelleRiville("Varattuja pöytiä: " + varattujaPoytia);
             varattujaPoytia = KasvataVarattujaPoytia(varattujaPoytia);
@@ -61,7 +61,7 @@ namespace FunktiotHarjoituksia
             TulostaUudelleRiville("Varattuja pöytiä: " + varattujaPoytia);
             TulostaUudelleRiville("------------------------------------");
 
-            // Kutsutaan KasvataVieraidenMaaraa()-metodia. Katso metodin
+            // Kutsutaan KasvataVieraidenMaaraa()-funktiota. Katso funktion
             // kuvauksesta mitä tekee. Esimerkki metodeissa käytettävistä
             // muuttujista. Edellisessä esimerkissä käytiin jo läpi, mutta
             // tämä on hieman erilainen.
@@ -75,12 +75,12 @@ namespace FunktiotHarjoituksia
         /// <summary>
         /// void TulostaOhjelmanNimi()
         /// 
-        /// Huomaa avainsana "void". Void tarkoittaa, että metodi
-        /// ei palauta mitään arvoa. Metodi siis vain suorittaa sille
+        /// Huomaa avainsana "void". Void tarkoittaa, että funktio
+        /// ei palauta mitään arvoa. Funktio siis vain suorittaa sille
         /// määritetyt tehtävät. Esimerkin tapauksessa tulostaa
         /// komentokehotteeseen ohjelman otsikon.
         /// </summary>
-        static void TulostaOhjelmanNimi()
+        public static void TulostaOhjelmanNimi()
         {
             Console.WriteLine("RUOKASALIN PAIKKALASKURI, versio 1.0");
         }
@@ -89,15 +89,15 @@ namespace FunktiotHarjoituksia
         /// int HaePoytienKokonaismaara()
         /// 
         /// Huomaa avainsana "int". Int on yksi ohjelmointikielen
-        /// perustyypeistä, joita muuttujat voivat olla. Metodi voi siis
+        /// perustyypeistä, joita muuttujat voivat olla. Funktio voi siis
         /// palauttaa minkä tahansa muuttujan tyypin, kun int-avainsana
         /// korvataan jollain muulla tyypillä, esimerkiksi string.
         /// 
-        /// Metodi voi palauttaa myös monimutkaisempia arvoja kuten olioita.
+        /// funktio voi palauttaa myös monimutkaisempia arvoja kuten olioita.
         /// Olioita käsitellään myöhemmin opintojakson luennoilla.
         /// </summary>
         /// <returns></returns>
-        static int HaePoytienKokonaismaara()
+        public static int HaePoytienKokonaismaara()
         {
             int poytienMaara = 12;
             return poytienMaara;
@@ -106,11 +106,11 @@ namespace FunktiotHarjoituksia
         /// <summary>
         /// string PalautaPoytienMaara()
         /// 
-        /// Esimerkki metodista, jossa aikaisemmin esitetyn esimerkin
+        /// Esimerkki funktiosta, jossa aikaisemmin esitetyn esimerkin
         /// sijaan palautetaan string-tietoptyyppi.
         /// </summary>
         /// <returns></returns>
-        static string PalautaPoytienMaara()
+        public static string PalautaPoytienMaara()
         {
             int poytienMaara = 12;
             return "Paikkoja yhteensa: " + poytienMaara + " kpl (vaihtoehtoinen)";
@@ -119,10 +119,10 @@ namespace FunktiotHarjoituksia
         /// <summary>
         /// void TulostaUudelleRiville(string tulostettavaMerkkijono)
         /// 
-        /// Huomaa metodin nimen perässä, sulkujen sisällä oleva
+        /// Huomaa funktion nimen perässä, sulkujen sisällä oleva
         /// "string" -perustyyppi ja "tulostettavaMerkkijono"-muuttuja.
         /// 
-        /// Metodiin voidaan antaa yksi tai useampi määrä parametreja,
+        /// Funktioon voidaan antaa yksi tai useampi määrä parametreja,
         /// jotka voivat olla ohjelmointikielen perustyyppejä tai monimutkaisempia
         /// tietotyyppejä. Esimerkissä annetaan vain yksi parametri mutta
         /// useamman parametrin tilanteessa jokainen erotetaan pilkulla.
@@ -133,7 +133,7 @@ namespace FunktiotHarjoituksia
         /// string      tulostettavaMerkkijono
         /// </summary>
         /// <param name="tulostettavaMerkkijono"></param>
-        static void TulostaUudelleRiville(string tulostettavaMerkkijono)
+        public static void TulostaUudelleRiville(string tulostettavaMerkkijono)
         {
             Console.WriteLine(tulostettavaMerkkijono);
         }
@@ -143,23 +143,23 @@ namespace FunktiotHarjoituksia
         /// 
         /// Huomaa, että parametrin nimi on sama kuin muuttujan, joka
         /// on esitelty pääohjelmassa. Tämän esimerkin tavoitteena on
-        /// havainnollistaa, että muuttuja ei ole sama kuin metodissa
+        /// havainnollistaa, että muuttuja ei ole sama kuin funktiossa
         /// käytettävä parametri.
         /// 
-        /// Metodissa parametrina annettu arvo kopioituu metodin käyttöön
+        /// Funktiossa parametrina annettu arvo kopioituu funktion käyttöön
         /// omaksi muutujakseen. Joten pääohjelmassa esitetty "varattujaPoytia"
         /// -muuttuja ja parametrina esitelty "varattujaPoytia"-parametri,
-        /// ovat kaksi eri asiaa. Metodissa parametrista tulee sen muuttuja.
+        /// ovat kaksi eri asiaa. Funktiossa parametrista tulee sen muuttuja.
         /// 
-        /// Tämän avulla metodissa voidaan käyttää samannimisä muuttujia kuin
+        /// Tämän avulla funktiossa voidaan käyttää samannimisä muuttujia kuin
         /// pääohjelmassa eivätkä ne sekoita toisiaan.
         /// 
         /// Esimerkissä parametrina annettua arvoa kasvatetaan ja palautetaan
-        /// uusi arvo sinne, missä metodia on kutsuttu.
+        /// uusi arvo sinne, missä funktiota on kutsuttu.
         /// </summary>
         /// <param name="varattujaPoytia"></param>
         /// <returns></returns>
-        static int KasvataVarattujaPoytia(int varattujaPoytia)
+        public static int KasvataVarattujaPoytia(int varattujaPoytia)
         {
             return varattujaPoytia + 1;
         }
@@ -167,23 +167,23 @@ namespace FunktiotHarjoituksia
         /// <summary>
         /// void KasvataVieraidenMaaraa(int vieraidenMaara)
         /// 
-        /// Huomaa metodissa, että parametrina annetun arvon nimi
+        /// Huomaa funktiossa, että parametrina annetun arvon nimi
         /// on sama kuin pääohjelman muuttuja. 
         /// 
         /// Esimerkissä yritetään kasvattaa vieraiden lukumäärää.
         /// Vaikka lukua kasvatetaan niin sen muutokset eivät heijastu
-        /// pääohjelmaan. Tällöin huomataan, että metodissa muuttujat
+        /// pääohjelmaan. Tällöin huomataan, että funktiossa muuttujat
         /// ovat omia ja voivat olla saman nimisiä kuin pääohjelmassa.
         /// 
-        /// Kun metodiin lähetetään parametrina arvo, sen arvo kopioituu
+        /// Kun funktioon lähetetään parametrina arvo, sen arvo kopioituu
         /// alkuperäisestä muuttujasta. On myös tapoja vaikuttaa tähän
         /// käytökseen mutta niitä ei tässä harjoituksessa käydä läpi.
         /// </summary>
         /// <param name="vieraidenMaara"></param>
-        static void KasvataVieraidenMaaraa(int vieraidenMaara)
+        public static void KasvataVieraidenMaaraa(int vieraidenMaara)
         {
             vieraidenMaara = vieraidenMaara + 1;
-            Console.WriteLine("(Tämä tulostuu metodista) Vieraiden lukumäärä: " + vieraidenMaara);
+            Console.WriteLine("(Tämä tulostuu funktiosta) Vieraiden lukumäärä: " + vieraidenMaara);
         }
     }
 }
