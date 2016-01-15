0c5ae14052cbd1deeebbdabbef571ebbb30356ad
Turo Nylund <turo.nylund@outlook.com>
2016-01-15 21:17:42 +0200

    Refaktoroitu harjoitus

--- ListaRefaktorointi/ListaRefaktorointi/Program.cs
+++ ListaRefaktorointi/ListaRefaktorointi/Program.cs
@@ -7,25 +7,39 @@ namespace ListaRefaktorointi
     {
         static void Main(string[] args)
         {
-            Tilaus tilaus = new Tilaus();
-
             List<Tilaus> tilaukset = new List<Tilaus>();
 
-            tilaus.UusiTilaus(tilaukset, "Pöytä 1", 39.90);
-            Console.WriteLine(tilaukset.Count);
+            var listankoko = tilaukset.Count;
 
-            tilaus.UusiTilaus(tilaukset, "Pöytä 2", 12.50);
-            Console.WriteLine(tilaukset.Count);
 
-            tilaus.UusiTilaus(tilaukset, "Pöytä 1", 104.30);
-            Console.WriteLine(tilaukset.Count);
+            Tilaus tilaus1 = new Tilaus(6543, "Pöytä 1");
+            Tilausrivi tilausrivi1 = new Tilausrivi("Lehtipihvi", 12.5, 1);
+            Tilausrivi tilausrivi2 = new Tilausrivi("Nakit ja muussi", 9.5, 1);
+            tilaus1.LisaaTilausrivi(tilausrivi1);
+            tilaus1.LisaaTilausrivi(tilausrivi2);
 
-            for (int i = 0; i < tilaukset.Count; i++)
-            {
-                Console.WriteLine(tilaukset[i].tilausnro);
-                Console.WriteLine(tilaukset[i].poyta);
-                Console.WriteLine(tilaukset[i].loppusumma);
-            }
+            Console.WriteLine(tilaus1.PalautaLoppusumma());
+
+
+            tilaukset.Add(tilaus1);
+
+            //List<Tilaus> tilaukset = new List<Tilaus>();
+
+            //tilaus.UusiTilaus(tilaukset, "Pöytä 1", 39.90);
+            //Console.WriteLine(tilaukset.Count);
+
+            //tilaus.UusiTilaus(tilaukset, "Pöytä 2", 12.50);
+            //Console.WriteLine(tilaukset.Count);
+
+            //tilaus.UusiTilaus(tilaukset, "Pöytä 1", 104.30);
+            //Console.WriteLine(tilaukset.Count);
+
+            //for (int i = 0; i < tilaukset.Count; i++)
+            //{
+            //    Console.WriteLine(tilaukset[i].tilausnro);
+            //    Console.WriteLine(tilaukset[i].poyta);
+            //    Console.WriteLine(tilaukset[i].loppusumma);
+            //}
 
             Console.ReadKey();
         }
--- ListaRefaktorointi/ListaRefaktorointi/Tilaus.cs
+++ ListaRefaktorointi/ListaRefaktorointi/Tilaus.cs
@@ -1,43 +1,80 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ListaRefaktorointi
 {
-    public class Tilaus
+    /*
+    Tärkeä muistisääntö on, että luokan pitäisi käsitellä vain yhtä
+    asiaa eli sillä on yksi syy muuttua. Kun luokan vastuulla on useampi
+    kuin yksi tehtävä, syntyy usein monimutkaisia rakenteita, jotka myöhemmin
+    johtavat ongelmiin.
+    */
+    public class Tilaus // Luokan nimi viittaa tilaukseen, joten keskitytään siihen
     {
-        public int tilausnro;
-        public string poyta;
-        public double loppusumma;
-        public List<Tilausrivi> tilausrivit;
+        public int tilausnro; // hyvä luokan attribuutti
+        public string poyta; // samoin tämä
+        public List<Tilausrivi> tilausrivit; // hyvä attribuutti
 
-        public Tilaus()
+        public Tilaus(int tilausnumero, string poyta)
         {
+            this.tilausnro = tilausnumero;
+            this.poyta = poyta;
             tilausrivit = new List<Tilausrivi>();
         }
 
-        public void UusiTilaus(List<Tilaus> ptilaukset, string ppoyta, double ploppusumma)
+        public void LisaaTilausrivi(Tilausrivi rivi)
         {
-            int uusitilausnro = VapaaTilausnumero(ptilaukset);
-
-            Tilaus uusitilaus = new Tilaus();
-
-            uusitilaus.tilausnro = uusitilausnro;
-            uusitilaus.poyta = ppoyta;
-            uusitilaus.loppusumma = ploppusumma;
-
-            ptilaukset.Add(uusitilaus);
+            tilausrivit.Add(rivi);
         }
 
-        private int VapaaTilausnumero(List<Tilaus> ptilaukset)
+        public double PalautaLoppusumma()
         {
-            int suurintilausnro = 0;
-            for (int i = 0; i < ptilaukset.Count; i++)
+            double loppusumma = 0;
+            foreach (Tilausrivi rivi in tilausrivit)
             {
-                if (ptilaukset[i].tilausnro > suurintilausnro)
-                {
-                    suurintilausnro = ptilaukset[i].tilausnro;
-                }
+                loppusumma += rivi.ahinta;
             }
-            return suurintilausnro + 1;
+            return loppusumma;
         }
+
+        /*
+        Tärkeä samankaltainen muistisääntö metodeille on, että ne tekevät yhden asian.
+        Sama ohjeistus kuin luokkien kanssa. Metodin nimen pitää olla kuvaava sen tekemälle
+        asialle.
+
+        UusiTilaus siis luo uuden Tilaus-olion, täyttää parametreina annetut tiedot, hakee
+        uuden tilausnumeron ja tallentaa sen listaan.
+
+        Voidaan miettiä uudestaan näin päin:
+        - UusiTilaus??? Kun luot tämän tilaus luokan niin eikös rakentaja tee juuri saman asian?
+        - Metodissa luodaan sama luokka kuin missä metodi on. Rakenne aiheuttaa helposti ongelmia.
+        - Metodissa lisätään tilaus listaan, joka sisältää tilauksia eli ns. tietokanta. Tässä
+          tärkeä kysymys on: Miksi Tilaus-luokka tietää miten ja minne tallennetaa? Nämä kannattaa erottaa.
+        */
+        //public void UusiTilaus(List<Tilaus> ptilaukset, string ppoyta, double ploppusumma)
+        //{
+        //    int uusitilausnro = VapaaTilausnumero(ptilaukset);
+
+        //    Tilaus uusitilaus = new Tilaus();
+
+        //    uusitilaus.tilausnro = uusitilausnro;
+        //    uusitilaus.poyta = ppoyta;
+        //    uusitilaus.loppusumma = ploppusumma;
+
+        //    ptilaukset.Add(uusitilaus);
+        //}
+
+        //private int VapaaTilausnumero(List<Tilaus> ptilaukset)
+        //{
+        //    int suurintilausnro = 0;
+        //    for (int i = 0; i < ptilaukset.Count; i++)
+        //    {
+        //        if (ptilaukset[i].tilausnro > suurintilausnro)
+        //        {
+        //            suurintilausnro = ptilaukset[i].tilausnro;
+        //        }
+        //    }
+        //    return suurintilausnro + 1;
+        //}
     }
 }
